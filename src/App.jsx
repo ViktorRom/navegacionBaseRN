2cf913fafa482e4b753ddb2c1b285f7a81ff6547
Victor Romero <vromero@bsigeonet.com.mx>
2023-05-17 21:43:10 -0600

    Creation of bases for the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () =>{
+
+  return(
+    <View>
+      <Text>
+       No te iba a durar toda la vida Ivan
+      </Text>
+    </View>
+  )
+}
+export default App;
